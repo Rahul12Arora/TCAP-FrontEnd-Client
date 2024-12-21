34d9427658b792aa30202a050eaf6c7d3e64db75
Rahul Arora <rahul.012.arora@gmail.com>
2024-12-21 17:38:38 +0530

    test

--- frontend/src/Config/socketConfig.js
+++ frontend/src/Config/socketConfig.js
@@ -3,3 +3,10 @@ import { io } from "socket.io-client";
 export const dmChatSocket = io('/dmchat', { autoConnect: false });
 export const groupChatSocket = io('/groupChat', { autoConnect: false });
 export const notificationSocket = io('/notification', { autoConnect: false });
+
+// // plain object
+// const socket = io({
+//     auth: {
+//       token: "abc"
+//     }
+//   })
