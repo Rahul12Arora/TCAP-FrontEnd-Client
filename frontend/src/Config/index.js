d1a066f25d85bb9530a1cca66a286cf703f1159c
Rahul Arora <rahul.012.arora@gmail.com>
2025-01-19 23:48:45 +0530

    Update index.js

--- frontend/src/Config/index.js
+++ frontend/src/Config/index.js
@@ -1,5 +1,6 @@
 const dev = {
-    apiUrl: "http://localhost:8080/",
+    // apiUrl: "http://localhost:8080/", // original for local system
+    apiUrl: "http://13.201.129.255:8080/", // for ec2 instance
 }
 let config = dev;
 export default config;
